66032b6db996700b3c9a616c686705c8035e6d6b
Justin Schnurer <justinschnurer@gmail.com>
2020-03-29 22:22:41 -0400

    added mm8

--- src/Menu.svelte
+++ src/Menu.svelte
@@ -11,6 +11,7 @@
 
 <h2>Might and Magic Trainers</h2>
 <div class="game-selection">
-<Link to={window.__settings.hostDir + "/trainers-list/mm6"}>Might & Magic VI</Link>
-<Link to={window.__settings.hostDir + "/trainers-list/mm7"}>Might & Magic VII</Link>
+<Link to={window.__settings.hostDir + "/trainers-list/mm6"}>Might & Magic VI: The Mandate of Heaven</Link>
+<Link to={window.__settings.hostDir + "/trainers-list/mm7"}>Might & Magic VII: For Blood and Honor</Link>
+<Link to={window.__settings.hostDir + "/trainers-list/mm8"}>Might & Magic VIII: Day of the Destroyer</Link>
 </div>
